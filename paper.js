0ea68abfa77e0e29cd5462b3dc8de2d02c29a3fb
saptaparnaMaity <75085120+saptaparnaMaity@users.noreply.github.com>
2021-01-06 07:28:35 -0800

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,16 @@
+class Paper extends BaseClass {
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("paper.png.png");
+    }
+  
+    display() {
+     
+        super.display();
+      }
+  }
+  
+
+
+
+    
